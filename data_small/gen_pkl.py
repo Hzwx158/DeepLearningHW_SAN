cc1b83751c09a3e13c196ee506799521aded3c89
Birdman0626 <3440859713@qq.com>
2024-05-16 20:59:37 +0800

    研究了这个文件的用法

--- data_small/gen_pkl.py
+++ data_small/gen_pkl.py
@@ -4,11 +4,21 @@ from tqdm import tqdm
 import cv2
 import pickle as pkl
 
+#用来生成对应的训练/测试所需要的pkl文件
+#使用方法：修改path和out即可
+#代码原理没有细究
+#TODO: 有时间的话稍微写一下原理
 
-image_path = '/Users/tal/Documents/Tal/myWork/公式识别/code/WAP/data/off_image_train'
-image_out = 'train_image.pkl'
-laebl_path = 'train_hyb'
-label_out = 'train_label.pkl'
+#image_path = '/Users/tal/Documents/Tal/myWork/公式识别/code/WAP/data/off_image_train'
+#image_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\dataset-from-wan\off_image_train\off_image_train"
+image_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\14_test_images\14_test_images"
+#image_out = './train_image.pkl'
+image_out = './test_image.pkl'
+#label_path = 'train_hyb'
+label_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\test_caption.txt"
+#label_out = './train_label.pkl'
+label_out = './test_label.pkl'
+
 
 images = glob.glob(os.path.join(image_path, '*.bmp'))
 image_dict = {}
@@ -22,7 +32,7 @@ for item in tqdm(images):
 with open(image_out,'wb') as f:
     pkl.dump(image_dict, f)
 
-labels = glob.glob(os.path.join(laebl_path, '*.txt'))
+labels = glob.glob(os.path.join(label_path, '*.txt'))
 label_dict = {}
 
 for item in tqdm(labels):
