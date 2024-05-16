78953123820f6e266afc52cd3d883cc4b2f29328
Birdman0626 <3440859713@qq.com>
2024-05-16 21:47:40 +0800

    已成功利用此文件生成pkl

--- data_small/gen_pkl.py
+++ data_small/gen_pkl.py
@@ -4,39 +4,52 @@ from tqdm import tqdm
 import cv2
 import pickle as pkl
 
-#用来生成对应的训练/测试所需要的pkl文件
-#使用方法：修改path和out即可
-#代码原理没有细究
-#TODO: 有时间的话稍微写一下原理
+'''
+    用来生成对应的训练/测试所需要的pkl文件
+    使用方法: 修改path和out即可
+    请特别注意不要把目录写错(通过assert进行了检查)
+'''
+
 
 #image_path = '/Users/tal/Documents/Tal/myWork/公式识别/code/WAP/data/off_image_train'
 #image_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\dataset-from-wan\off_image_train\off_image_train"
-image_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\14_test_images\14_test_images"
+#image_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\a_matched_test_set\14_test_images\14_test_images"
+#image_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\HME100K\HME100K\train\train_images"
+image_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\HME100K\HME100K\test\test_images"
+
 #image_out = './train_image.pkl'
 image_out = './test_image.pkl'
+
 #label_path = 'train_hyb'
-label_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\test_caption.txt"
+#label_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\HME100K\HME100K\train"
+label_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\HME100K\HME100K\test"
+#label_path = r"E:\pythonwork\deep_learning\big_homework\SAN-main\data\a_matched_test_set"
+
 #label_out = './train_label.pkl'
 label_out = './test_label.pkl'
 
 
-images = glob.glob(os.path.join(image_path, '*.bmp'))
+subfix = 'jpg'
+images = glob.glob(os.path.join(image_path, f'*.{subfix}'))
 image_dict = {}
-
+assert len(images)!=0, "empty images"
 for item in tqdm(images):
 
     img = cv2.imread(item)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    image_dict[os.path.basename(item).replace('_0.bmp','')] = img
+    #image_dict[os.path.basename(item).replace(f'_0.{subfix}','')] = img
+    image_dict[os.path.basename(item).replace(f'.{subfix}','')] = img
 
 with open(image_out,'wb') as f:
     pkl.dump(image_dict, f)
 
 labels = glob.glob(os.path.join(label_path, '*.txt'))
+assert len(labels)!=0, "empty labels!"
 label_dict = {}
 
+#测试集中含有中文顿号，需要用utf-8， 否则会报错
 for item in tqdm(labels):
-    with open(item) as f:
+    with open(item, 'r', encoding='utf-8') as f:
         lines = f.readlines()
     label_dict[os.path.basename(item).replace('.txt','')] = lines
 
